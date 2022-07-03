5ed5c11eb5a83f06f9788a984bcf0ec7f6a4ac53
Ezekiel Harvey <zekeharvey@outlook.com>
2022-07-02 22:41:55 -0400

    AddButton Update

--- src/Screens/CardsScreen/CardsScreen.js
+++ src/Screens/CardsScreen/CardsScreen.js
@@ -9,27 +9,30 @@ import {
   SafeAreaView,
   TextInput,
 } from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import Lottie from 'lottie-react-native';
+import {Swipeable} from 'react-native-gesture-handler';
 
 const DATA = [
   {
     id: 1,
     title: 'Card 1',
-    //subtitle: 'This is a test subtitle',
+    subtitle: 'This is a test subtitle',
   },
   {
     id: 2,
     title: 'Card 2',
-    //subtitle: 'This is a test subtitle',
+    subtitle: 'This is a test subtitle',
   },
   {
     id: 3,
     title: 'Card 3',
-    //subtitle: 'This is a test subtitle',
+    subtitle: 'This is a test subtitle',
   },
   {
     id: 4,
     title: 'Card 4',
-    //subtitle: 'This is a test subtitle',
+    subtitle: 'This is a test subtitle',
   },
 ];
 
@@ -38,33 +41,38 @@ function CardsScreen() {
   const [isRender, setIsRender] = useState(false);
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [inputText, setInputText] = useState();
+  const [inputSubText, setInputSubText] = useState();
   const [editItem, setEditItem] = useState();
 
   const onPressItem = item => {
     setIsModalVisible(true);
     setInputText(item.title);
+    setInputSubText(item.subtitle);
     setEditItem(item.id);
-  }
+  };
 
   const renderItem = ({item, index}) => {
     return (
-      <TouchableOpacity style={styles.item} onPress={() => onPressItem(item)}>
-        <Text style={styles.text}>{item.title}</Text>
-        {/* <Text style={styles.subtext}>{item.subtitle}</Text> */}
-      </TouchableOpacity>
+      <Swipeable renderRightActions={renderRightActions}>
+        <TouchableOpacity style={styles.item} onPress={() => onPressItem(item)}>
+          <Text style={styles.text}>{item.title}</Text>
+          <Text style={styles.subtext}>{item.subtitle}</Text>
+        </TouchableOpacity>
+      </Swipeable>
     );
   };
 
-  const handleEditItem = (editItem) => {
+  const handleEditItem = editItem => {
     const newData = data.map(item => {
       if (item.id == editItem) {
         item.title = inputText;
+        item.subtitle = inputSubText;
         return item;
       }
       return item;
-    })
+    });
     setData(newData);
-    setIsRender(!isRender)
+    setIsRender(!isRender);
   };
 
   const onPressSaveEdit = () => {
@@ -72,12 +80,17 @@ function CardsScreen() {
     setIsModalVisible(false);
   };
 
+  const addCard = () => {
+    console.log('Pressed');
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <FlatList
         data={data}
-        keyExtractor={(item) => item.id.toString()}
+        keyExtractor={item => item.id.toString()}
         renderItem={renderItem}
+        contentInset={{right: 0, top: 0, left: 0, bottom: 10}}
         extraData={isRender}
       />
       <Modal
@@ -86,12 +99,42 @@ function CardsScreen() {
         onRequestClose={() => setIsModalVisible(false)}>
         <View style={styles.modalView}>
           <Text style={styles.text}>Change Text: </Text>
+          <View style={{width: '100%'}}>
+            <Text
+              style={{
+                textAlign: 'left',
+                marginLeft: 35,
+                fontWeight: 'bold',
+                fontSize: 18,
+              }}>
+              Title
+            </Text>
+          </View>
           <TextInput
             style={styles.textInput}
             onChangeText={title => setInputText(title)}
             defaultValue={inputText}
             editable={true}
             multiline={false}
+            maxLength={30}
+          />
+          <View style={{width: '100%'}}>
+            <Text
+              style={{
+                textAlign: 'left',
+                marginLeft: 35,
+                fontWeight: 'bold',
+                fontSize: 18,
+              }}>
+              Description
+            </Text>
+          </View>
+          <TextInput
+            style={styles.textInput}
+            onChangeText={subtitle => setInputSubText(subtitle)}
+            defaultValue={inputSubText}
+            editable={true}
+            multiline={true}
             maxLength={1000}
           />
           <TouchableOpacity
@@ -101,6 +144,13 @@ function CardsScreen() {
           </TouchableOpacity>
         </View>
       </Modal>
+      <TouchableOpacity style={styles.addButton} onPress={addCard}>
+        <Lottie
+          source={require('../../../assets/AddButton.json')}
+          autoPlay
+          loop
+        />
+      </TouchableOpacity>
     </SafeAreaView>
   );
 }
@@ -162,6 +212,24 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     padding: 15,
     marginTop: 20,
+  },
+  addButton: {
+    height: 75,
+    width: 75,
+    borderRadius: 35,
+    position: 'absolute',
+    justifyContent: 'center',
+    alignItems: 'center',
+    bottom: 0,
+    margin: 10,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    right: 0,
   },
 });
 
